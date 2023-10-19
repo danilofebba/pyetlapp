14a449fdc4be9cb777f369f552fdb5aaf9091bb1
Danilo de Febba Santos <46852352+danilofebba@users.noreply.github.com>
2023-10-18 23:30:04 -0300

    version 1.0

--- support/python_venv.sh
+++ support/python_venv.sh
@@ -8,4 +8,5 @@ echo -e "c.NotebookApp.token = ''" >> ~/.jupyter/jupyter_lab_config.py
 echo -e "c.NotebookApp.password = 'argon2:\$argon2id\$v=19\$m=10240,t=10,p=8\$635AifC1hEtV+BPmOyHphQ\$lP1zV4+cd0BODPyv3IW7h4j/3PKd8cnAgJGoebpsqwA'" >> ~/.jupyter/jupyter_lab_config.py
 python3 -m pip install --upgrade numpy pyarrow pandas
 python3 -m pip install --upgrade pyspark
+python3 -m pip install --upgrade "psycopg[binary]"
 #jupyter lab --ip=0.0.0.0 --allow-root
